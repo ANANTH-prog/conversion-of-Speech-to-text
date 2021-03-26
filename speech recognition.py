21ed0f308732722aa08c0d859d54b251a3382659
ANANTH-prog <69955240+ANANTH-prog@users.noreply.github.com>
2021-03-26 11:21:33 +0530

    Update speech recognition.py

--- speech recognition.py
+++ speech recognition.py
@@ -1,40 +1,88 @@
 import speech_recognition as sr
-import pandas as pd
-import numpy as np
-
 r = sr.Recognizer()
 
-audio_file = sr.AudioFile("C:/Users/ANANTH/Desktop/preamble.wav")
+audio_file = sr.AudioFile("C:/Users/ANANTH/Desktop/imp1.wav")
 
 with audio_file as source:
     r.adjust_for_ambient_noise(source)
     audio = r.record(source)
 result = r.recognize_google(audio)
 
-with open("C:/Users/ANANTH/Desktop/preamble1.txt",mode ="w") as file:
+with open("C:/Users/ANANTH/Desktop/imp2.txt",mode ="w") as file:
     file.write("Recognized text:")
     file.write("\n")
     file.write(result)
-    print("Hurray! conversion is complete")
+    print("conversion is complete")
+##Sentiment analysis
+import string
+from collections import Counter
+import nltk
+nltk.download('vader_lexicon')
+
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+text = open('C:/Users/ANANTH/Desktop/imp1.txt').read()
+lower_case = text.lower()
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+
+# Using word_tokenize because it's faster than split()
+tokenized_words = word_tokenize(cleaned_text, "english")
+
+#stopwords
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+# Removing stop words from the tokenized words list
+final_words = []
+for word in tokenized_words:
+    if word not in stop_words:
+        final_words.append(word)
+#emotional list        
+emotion_list = []
+with open('C:/Users/ANANTH/Desktop/emotions.txt', 'r') as file:
+    for line in file:
+        clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+
+        if word in final_words:
+            emotion_list.append(emotion)
+
+print(emotion_list)
+w = Counter(emotion_list)
+print(w)
+
+# Plotting the emotions on the graph
+
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
+
+
+def sentiment_analyse(sentiment_text):
+    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+    print(score)
+    if score['neg'] > score['pos']:
+        print("Negative Sentiment")
+    elif score['neg'] < score['pos']:
+        print("Positive Sentiment")
     
 
-##
-from textblob import TextBlob
-import nltk
-nltk.download("punkt")
-text =open("C:/Users/ANANTH/Desktop/test.txt", "r")
-print(text.read())
-##create a textblob object
-obj=TextBlob(str(text))
-#this returns a value between -1 and 1
-sentiment=obj.sentiment.polarity
-print(sentiment)
-#doing sentiment analysis split them in to positive, Negative, Neutral
-if sentiment == 0:
-    print("the text is neutral")
-elif sentiment > 0:
-    print("text is positive")
-else:
-    print("the text is negative")
+
+sentiment_analyse(cleaned_text)
+
     
 
